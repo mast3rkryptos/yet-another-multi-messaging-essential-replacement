9d2721d8013b59f9f7d401e1644071a0b8d1ea7a
Schive <gwschive@ccanet.rockwellcollins.com>
2019-03-02 13:46:45 -0600

    Working on client UI

--- client.py
+++ client.py
@@ -1,5 +1,9 @@
 import re
 import socket
+import Tkinter as tk
+
+from ScrolledText import ScrolledText
+from Tkinter import *
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
@@ -7,6 +11,8 @@ BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
 
 
+# TODO
+# - Output to log file
 
 # - Support the following commands
 #   - cc: Create Channel
@@ -62,7 +68,58 @@ def main():
 
     print "[client] received data:", data
 
-#main()
 
-# TODO
-# - Output to log file
+class ClientApplication(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid(sticky=tk.N + tk.S + tk.E + tk.W)
+        self.menu = Menu()
+        self.textDisplay = ScrolledText()
+        self.textEntry = Entry()
+        self.send = Button()
+        self.create_widgets()
+
+    def create_widgets(self):
+        top = self.winfo_toplevel()
+        top.rowconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
+        # Menu
+        fileMenu = Menu(self.menu)
+        fileMenu.add_command(label='Configure', command=configure)
+        fileMenu.add_command(label='Connect', command=connect)
+        self.menu.add_cascade(label='File', menu=fileMenu)
+        top.config(menu=self.menu)
+
+        # Text Display
+        self.textDisplay.grid(row=0, column=0, columnspan=2, sticky=tk.N + tk.S + tk.E + tk.W)
+
+        # Text Entry
+        self.textEntry.bind('<Return>', send_event)
+        self.textEntry.grid(row=1, column=0, sticky=tk.N + tk.S + tk.E + tk.W)
+
+        # Send button
+        self.send = Button(text="Send", command=send)
+        self.send.grid(row=1, column=1, sticky=tk.N + tk.S + tk.E + tk.W)
+
+
+def configure():
+    print "Configure"
+
+
+def connect():
+    print "Connect"
+
+
+def send():
+    print "Send"
+
+def send_event(event):
+    send()
+
+if __name__ == "__main__":
+    ca = ClientApplication()
+    ca.master.title('YAMMER')
+    ca.mainloop()
