5b214eb5ffe5a682068649160634f30fe3f4bfde
mast3rkryptos <mast3rkryptos@users.noreply.github.com>
2019-03-05 15:33:33 -0600

    Working on getting text entry working

--- client.py
+++ client.py
@@ -1,15 +1,11 @@
-import re
-import socket
-import Tkinter as tk
-
-from ScrolledText import ScrolledText
-from Tkinter import *
-
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
-BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
-
+try:
+    # for Python2
+    import Tkinter as tk
+    import ScrolledText as tkst
+except ImportError:
+    # for Python3
+    import tkinter as tk
+    import tkinter.scrolledtext as tkst
 
 # TODO
 # - Output to log file
@@ -29,100 +25,60 @@ MESSAGE = "Hello, World!"
 #   - y:  Yell
 #   - w:  Whisper
 #   - r:  Reply
-def parse_command(user_cmd):
-    p = re.compile("^/([a-zA-Z]+) ([\d\s\w]*$)")
-    m = p.match(user_cmd)
-    cmd = m.group(1).lower()
 
-
-def parse_input(user_input):
-    if user_input.startswith("/"):
-        parse_command(user_input)
-    else:
-        # This is just standard message, send it to the active channel
-        send_message(user_input)
-
-
-def send_message(msg):
-    print "sent: ", msg
-
-
-def main():
-    print "Client - Finished"
-    exit()
-    s = " "
-    while " " in s:
-        s = raw_input("Username (no spaces):")
-    username = s
-    print "Welcome, " + username
-    while True:
-        s = raw_input("-<>")
-        parse_input(s)
-
-    print "[client] Starting up"
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((TCP_IP, TCP_PORT))
-    s.send(MESSAGE)
-    data = s.recv(BUFFER_SIZE)
-    s.close()
-
-    print "[client] received data:", data
-
-
-class ClientApplication(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.grid(sticky=tk.N + tk.S + tk.E + tk.W)
-        self.menu = Menu()
-        self.textDisplay = ScrolledText(state='disabled')
-        self.textEntry = Entry()
-        self.send = Button()
-        self.create_widgets()
-
-    def create_widgets(self):
-        top = self.winfo_toplevel()
-        top.rowconfigure(0, weight=1)
-        top.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=1)
-
-        # Menu
-        fileMenu = Menu(self.menu)
-        fileMenu.add_command(label='Configure', command=configure)
-        fileMenu.add_command(label='Connect', command=connect)
-        self.menu.add_cascade(label='File', menu=fileMenu)
-        top.config(menu=self.menu)
-
-        # Text Display
-        self.textDisplay.grid(row=0, column=0, columnspan=2, sticky=tk.N + tk.S + tk.E + tk.W)
-
-        # Text Entry
-        self.textEntry.bind('<Return>', send_event)
-        self.textEntry.grid(row=1, column=0, sticky=tk.N + tk.S + tk.E + tk.W)
-
-        # Send button
-        self.send = Button(text="Send", command=send)
-        self.send.grid(row=1, column=1, sticky=tk.N + tk.S + tk.E + tk.W)
-
-
-def configure():
+def do_configure():
     print "Configure"
 
 
-def connect():
+def do_connect():
     print "Connect"
 
 
-def send():
+def do_send():
     print "Send"
-    self.textDisplay.
+    text = str(frame.textEntry.get('1.0', tk.END)).strip()
+    if text != '':
+        write_to_display(text + '\n')
+    frame.textEntry.delete('1.0', tk.END)
 
 
 def send_event(event):
-    send()
+    do_send()
 
 
-if __name__ == "__main__":
-    ca = ClientApplication()
-    ca.master.title('YAMMER')
-    ca.mainloop()
+def write_to_display(text):
+    frame.textDisplay.config(state=tk.NORMAL)
+    frame.textDisplay.insert(tk.END, text)
+    frame.textDisplay.config(state=tk.DISABLED)
+
+root = tk.Tk()
+root.title("YAMMER")
+frame = tk.Frame(root)
+frame.grid(sticky=tk.N + tk.S + tk.E + tk.W)
+frame.winfo_toplevel().rowconfigure(0, weight=1)
+frame.winfo_toplevel().columnconfigure(0, weight=1)
+frame.rowconfigure(0, weight=1)
+frame.columnconfigure(0, weight=1)
+
+# Menu
+#frame.menu = tk.Menu()
+#frame.menu.add_command(label='Configure', command=do_configure)
+#frame.menu.add_command(label='Connect', command=do_connect)
+#frame.menu.add_cascade(label='File', menu=fileMenu)
+#frame.winfo_toplevel().config(menu=self.menu)
+
+# Test Display
+frame.textDisplay = tkst.ScrolledText(state=tk.DISABLED, wrap=tk.WORD)
+frame.textDisplay.grid(row=0, column=0, columnspan=2, sticky=tk.N + tk.S + tk.E + tk.W)
+
+# Text Entry
+frame.textEntry = tkst.ScrolledText(height=5)
+#self.textEntry.bind('<Shift-Return>')
+frame.bind('<Return>', send_event)
+frame.textEntry.grid(row=1, column=0, sticky=tk.N + tk.S + tk.E + tk.W)
+
+# Send Button
+frame.send = tk.Button(text="Send", command=do_send)
+frame.send.grid(row=1, column=1, sticky=tk.N + tk.S + tk.E + tk.W)
+
+root.mainloop()
--- test.py
+++ test.py
@@ -1,24 +1,37 @@
-#!/usr/bin/env python
-import Tkinter as tk
+try:
+    # for Python2
+    import Tkinter as tk
+    import ScrolledText as tkst
+except ImportError:
+    # for Python3
+    import tkinter as tk
+    import tkinter.scrolledtext as tkst
 
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.grid(sticky=tk.N + tk.S + tk.E + tk.W)
-        self.create_widgets()
-        self.quit = tk.Button()
-
-    def create_widgets(self):
-        top = self.winfo_toplevel()
-        top.rowconfigure(0, weight=1)
-        top.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
-        self.columnconfigure(0, weight=1)
-        self.quit = tk.Button(self, text='Quit', command=self.quit)
-        self.quit.grid(row=0, column=0, sticky=tk.N + tk.S + tk.E + tk.W)
+def test():
+    edit_space.insert('insert', "Test")
 
 
-app = Application()
-app.master.title('Sample application')
-app.mainloop()
+root = tk.Tk()
+root.title("ScrolledText")
+frame = tk.Frame(root, bg='brown')
+frame.pack(fill='both', expand='yes')
+button = tk.Button(master=frame, text="Test", command=test)
+button.pack()
+edit_space = tkst.ScrolledText(
+    master = frame,
+    wrap   = 'word',  # wrap text at full words only
+    width  = 25,      # characters
+    height = 10,      # text lines
+    bg='beige'        # background color of edit area
+)
+# the padx/pady space will form a frame
+edit_space.pack(fill='both', expand=True, padx=8, pady=8)
+mytext = '''\
+Man who drive like hell, bound to get there.
+Man who run in front of car, get tired.
+Man who run behind car, get exhausted.
+The Internet: where men are men, women are men, and children are FBI agents.
+'''
+edit_space.insert('insert', mytext)
+root.mainloop()
